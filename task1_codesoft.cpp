786b2e2bb96ebfd61775448bce826024849a68f6
MehulChoudhary <rajputmehul1122@gmail.com>
2024-07-26 15:26:42 +0530

    enter the commit message for your changes. Lines starting

--- task1_codesoft.cpp
+++ task1_codesoft.cpp
@@ -4,19 +4,21 @@ using namespace std;
 int main() {
   
   // to change the number everytime programme runned 
+  
   srand(time(NULL));
 
 // generation of random number using rand() function 
+
   int random_number = 1 + rand() % 100;
   int no_of_guess =0 ;
 
 int guess =0 ;
   
-
   // logical section 
+
   while (guess != random_number) {
    
-    cout << "Enter your guess: ";
+    cout << "Enter your guess  : ";
     cin >> guess;
 
     no_of_guess++;
